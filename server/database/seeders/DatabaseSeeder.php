2422684412a5c44371cb9e0f07dad3a0ce21b460
Bao Tran <buibaotran1997@gmail.com>
2021-01-29 10:08:25 +0700

    add estateStatusConstant and SubscribeStatusConstant

--- server/database/seeders/DatabaseSeeder.php
+++ server/database/seeders/DatabaseSeeder.php
@@ -13,6 +13,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call([
+            RoleSeeder::class,
+            UserSeeder::class,
+            CitySeeder::class,
+            ClientSeeder::class,
+            EstateSeeder::class
+        ]);
     }
 }
