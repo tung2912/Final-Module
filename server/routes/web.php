1e5fea6576553586e7545af08c83345f044742a6
Bao Tran <buibaotran1997@gmail.com>
2021-01-31 09:37:07 +0700

    fix

--- server/routes/web.php
+++ server/routes/web.php
@@ -13,11 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+
+
+Route::get('/admin',function () {
+    return view('admin.users.add');
+});
+
 //Route::get('{any}', function () {
 //    return view('client');
 //})->where('any','.*');
-
-
-Route::get('/',function () {
-    return view('admin.users.add');
-});
