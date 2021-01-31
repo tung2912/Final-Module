1323c7544fc6b5f9c4599225c6933ef06dd767b6
Bao Tran <buibaotran1997@gmail.com>
2021-01-31 11:16:58 +0700

    city Table

--- server/routes/web.php
+++ server/routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\CityController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,13 +20,9 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::get('/admin',function () {
-    return view('admin.users.add');
-});
 
-//Route::get('{any}', function () {
-//    return view('client');
-//})->where('any','.*');
+
+
 
 Route::get('/login',[LoginController::class,'showLogin'])->name('login');
 Route::post('/login',[LoginController::class,'login'])->name('admin.login');
@@ -34,6 +31,15 @@ Route::get('/logout',[LoginController::class,'logout'])->name('admin.logout');
 Route::middleware('auth')->prefix('admin')->group(function (){
 
     Route::get('/',[DashboardController::class,'dashboard'])->name('admin.dashboard');
+
+    Route::prefix('cities')->group(function () {
+        Route::get('/', [CityController::class, 'index'])->name('cities.index');
+        Route::get('/create', [CityController::class, 'create'])->name('cities.add');
+        Route::post('/create', [CityController::class, 'store'])->name('cities.store');
+        Route::get('{id}/edit', [CityController::class, 'edit'])->name('cities.edit');
+        Route::post('{id}/edit', [CityController::class, 'update'])->name('cities.update');
+        Route::get('{id}/delete', [CityController::class, 'delete'])->name('cities.delete');
+    });
 
 });
 
