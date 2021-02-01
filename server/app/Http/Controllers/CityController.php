c38303fca358a8833a5733c5c5c3768a30897c0e
Bao Tran <buibaotran1997@gmail.com>
2021-02-01 07:38:12 +0700

    add option user role for usersTable

--- server/app/Http/Controllers/CityController.php
+++ server/app/Http/Controllers/CityController.php
@@ -4,26 +4,29 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\CityRequest;
 use App\Models\City;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class CityController extends Controller
 {
     public function  index() {
         $cities = City::all();
-    //after create users table, add user_name to view
+
         return view('admin.cities.list', compact('cities'));
     }
 
     public function create() {
         $city = new City();
-        //after create users table, create option list user_name to view
-        return view('admin.cities.add');
+
+        $users = User::all();
+        return view('admin.cities.add', compact('users'));
     }
 
     public function store(CityRequest $request) {
         $city = new City();
 
         $city->name = $request->input('name');
+        $city->user_id = $request->input('user_id');
 
         $city->save();
 
@@ -33,16 +36,18 @@ class CityController extends Controller
     public function edit($id) {
 
         $city = City::findOrFail($id);
-        //after create users table, add user_name to view
-        return view('admin.cities.edit', compact('city'));
+        $users = User::all();
+
+        return view('admin.cities.edit', compact('city', 'users'));
 
 
     }
 
-    public function update(CityRequest $request, $id) {
+    public function update(Request $request, $id) {
         $city = City::findOrFail($id);
 
         $city->name = $request->input('name');
+        $city->user_id = $request->input('user_id');
 
         $city->save();
 
