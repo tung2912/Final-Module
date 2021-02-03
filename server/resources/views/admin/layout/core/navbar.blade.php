d36c432072ebc67ffca289de5141f46b5144cedd
Bao Tran <buibaotran1997@gmail.com>
2021-02-04 01:44:26 +0700

    add some detail to dashboard

--- server/resources/views/admin/layout/core/navbar.blade.php
+++ server/resources/views/admin/layout/core/navbar.blade.php
@@ -5,7 +5,7 @@
             <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
         </li>
         <li class="nav-item d-none d-sm-inline-block">
-            <a href="#" class="nav-link">Dashboard</a>
+            <a href="{{ route('admin.dashboard') }}" class="nav-link">Dashboard</a>
         </li>
     </ul>
     <form class="form-inline ml-3 mb-0">
