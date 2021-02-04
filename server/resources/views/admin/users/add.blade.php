7121b4682bb0c0fba0a88548dedf9a23f40ba478
= <=>
2021-02-04 14:07:53 +0700

    fix subscribe

--- server/resources/views/admin/users/add.blade.php
+++ server/resources/views/admin/users/add.blade.php
@@ -63,7 +63,7 @@
                             </div>
                             <div class="form-group">
                                 <input type="submit" value="Add" class="btn btn-success">
-                                <a href="#" class="btn btn-secondary">Back</a>
+                                <a href="{{route('users.index')}}" class="btn btn-secondary">Back</a>
                             </div>
                         </div>
                         <!-- /.card-body -->
